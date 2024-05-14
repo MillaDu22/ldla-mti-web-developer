b9bd42b35afc45e60c8126b1e776ce1bf3888327
Milla22 <marti.ludmilla@orange.fr>
2024-05-14 18:11:41 +0200

    maj gallery

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import "./card.scss";
 import { Link } from "react-router-dom";
 
-function Card({ id, cover, alt, title, informations, size }) {
+function Card({ id, cover, alt, title, informations }) {
     return (
         <div className= "card">
             <Link to={`/projet/${id}`} className="card-link">
                 <img src={cover} alt={alt} className="card-image" />
+                <h3 className="card-title">{title}</h3>
                 <div className="card-details">
-                    <h3 className="card-title">{title}</h3>
+                    <h4 className= "title-info">{title}</h4>
                     <p className="card-info">{informations}</p>
                 </div>
             </Link>
