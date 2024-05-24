b16c79bd62fa9d8ae1423415008f4ebb6f061fba
Milla22 <marti.ludmilla@orange.fr>
2024-05-25 00:12:29 +0200

    optimisation code carousel slides animations a l'arret lorsque les slides sont inactives

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -1,11 +1,12 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect, useCallback, useMemo } from "react";
 import CanvasWaves from "../CanvasWaves/index";
 import CanvasPaint from "../CanvasPaint/index";
 import "./carousel.scss";
 
-function Carousel(){
+const Carousel = () => {
     const [currentSlide, setCurrentSlide] = useState(0);
-    const slides = [
+
+    const slides = useMemo(() => [
         {
             id: 1,
             content: (
@@ -26,76 +27,108 @@ function Carousel(){
                     </div>
                     <h4 className="title-react">Pure CSS React ellipses Animation</h4>
                 </div>
-            )
+            ),
+            isCanvasActive: false
         },
         {
-        id:2, content: (
-            <div className ="container-loader">
-                <div className="ring">
-                    <h4>Loading...</h4>
+            id: 2,
+            content: (
+                <div className="container-loader">
+                    <div className="ring">
+                        <h4>Loading...</h4>
+                    </div>
                 </div>
-            </div>
-            )
+            ),
+            isCanvasActive: false
         },
         {
-            id:3, content: (
-                <div className ="container-background">
+            id: 3,
+            content: (
+                <div className="container-background">
                     <h4 className="title-back">Pure CSS Gradient Background Animation</h4>
                 </div>
-            )
+            ),
+            isCanvasActive: false
         },
         {
-            id:4, content: (
-                <div className ="container-text">
+            id: 4,
+            content: (
+                <div className="container-text">
                     <h4 className="text-anim">Pure Css Text Animation Effect</h4>
                 </div>
-            )
+            ),
+            isCanvasActive: false
         },
         {
-            id:5, content: (
-                <CanvasWaves />
-            )
+            id: 5,
+            content: <CanvasWaves />,
+            isCanvasActive: false
         },
         {
-            id:6, content: (
-                <CanvasPaint />
-            )
-        },
-    ];
+            id: 6,
+            content: <CanvasPaint />,
+            isCanvasActive: false
+        }
+    ], []);
 
-    // Function to handle next slide //
+    // Fonction pour passer à la diapositive suivante //
     const nextSlide = useCallback(() => {
         setCurrentSlide(currentSlide === slides.length - 1 ? 0 : currentSlide + 1);
     }, [currentSlide, slides.length]);
 
-    // Function to handle previous slide //
+    // Fonction pour passer à la diapositive précédente //
     const previousSlide = useCallback(() => {
         setCurrentSlide(currentSlide === 0 ? slides.length - 1 : currentSlide - 1);
     }, [currentSlide, slides.length]);
 
-    // Automatic slide change //
+    // Changement de diapositive automatique //
     useEffect(() => {
         const interval = setInterval(() => {
             nextSlide();
-        }, 5000); // Change slide every 5 seconds
+        }, 5000); // Changement de diapositive toutes les 5 secondes //
         return () => clearInterval(interval);
     }, [nextSlide]);
 
+    // Fonction pour activer/désactiver le canvas en fonction de la diapositive //
+    useEffect(() => {
+        const updateCanvasActivity = () => {
+            slides.forEach((slide, index) => {
+                if (index === currentSlide) {
+                    slide.isCanvasActive = true;
+                } else {
+                    slide.isCanvasActive = false;
+                }
+            });
+        };
+        updateCanvasActivity();
+    }, [currentSlide, slides]);
+
     return (
-        <div className ="carousel">
-            <h3 className ="title-carousel">Animations</h3>
+        <div className="carousel">
+            <h3 className="title-carousel">Animations</h3>
             <div className="container-arrows">
-                {slides.length > 1 && <i className="fa-solid fa-chevron-left left" onClick={previousSlide}></i>}
-                {slides.length > 1 && <i className="fa-solid fa-chevron-right right" onClick={nextSlide}></i>}
+                {slides.length > 1 && (
+                    <i className="fa-solid fa-chevron-left left" onClick={previousSlide}></i>
+                )}
+                {slides.length > 1 && (
+                    <i className="fa-solid fa-chevron-right right" onClick={nextSlide}></i>
+                )}
             </div>
             <div className="slide">
                 {slides.map((slide, index) => (
-                    <div  key={slide.id} className={`slide-content ${currentSlide === index ? "active" : "inactive"}`}>
+                    <div key={slide.id} className={`slide-content ${currentSlide === index ? "active" : "inactive"}`}>
                         {slide.content}
+                        {slide.isCanvasActive && (
+                            <>
+                                {slide.id === 5 && <CanvasWaves />}
+                                {slide.id === 6 && <CanvasPaint />}
+                            </>
+                        )}
                     </div>
                 ))}
             </div>
         </div>
-    )
-}
+    );
+};
+
 export default Carousel;
