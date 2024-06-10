6ce26737c31f54813ae2729ac91e75b16af2f51d
Milla22 <marti.ludmilla@orange.fr>
2024-06-10 15:04:14 +0200

    modification code skillbar

--- src/components/SkillBar/index.jsx
+++ src/components/SkillBar/index.jsx
@@ -2,40 +2,40 @@ import React, { useEffect, useState } from 'react';
 import "./SkillBar.css";
 
 const SkillBar = ({ skillName, percentage }) => {
-    const [progress, setProgress] = useState(0);
+    const [progress4, setProgress4] = useState(0);
 
     useEffect(() => {
-        const fillProgress = () => {
-            let currentProgress = 0;
-            const targetProgress = percentage;
-            const animationDuration = 2000; // 2 secondes//
-            const increment = (targetProgress / animationDuration) * 10;
-            const interval = setInterval(() => {
-                currentProgress += increment;
-                if (currentProgress >= targetProgress) {
-                    currentProgress = targetProgress;
-                    clearInterval(interval);
+        const fillProgress4 = () => {
+            let currentProgress4 = 0;
+            const targetProgress4 = percentage;
+            const animationDuration4 = 2000; // 2 secondes//
+            const increment = (targetProgress4 / animationDuration4) * 10;
+            const interval4 = setInterval(() => {
+                currentProgress4 += increment;
+                if (currentProgress4 >= targetProgress4) {
+                    currentProgress4 = targetProgress4;
+                    clearInterval(interval4);
                 }
-                setProgress(currentProgress);
+                setProgress4(currentProgress4);
             }, 10);
         };
-        fillProgress(); // Appel initial du remplissage progressif// 
-        const restartAnimation = () => {
-            const restartInterval = setInterval(() => {
-                setProgress(0); // Réinitialise la progrèss à 0
-                fillProgress(); // Redémarre l'animation
+        fillProgress4(); // Appel initial du remplissage progressif// 
+        const restartAnimation4 = () => {
+            const restartInterval4 = setInterval(() => {
+                setProgress4(0); // Réinitialise la progrèss à 0
+                fillProgress4(); // Redémarre l'animation
             }, 15000); // Toutes les 15 secondes
-            return () => clearInterval(restartInterval);
+            return () => clearInterval(restartInterval4);
         };
-        restartAnimation(); // Appel initial du redémarrage de l'animation     
+        restartAnimation4(); // Appel initial du redémarrage de l'animation     
     }, [percentage]);
 
     return (
         <div className="skill">
             <div className="skill-name">{skillName}</div>
             <div className="skill-bar">
-                <div className="skill-level" style={{ width: `${progress}%` }}>
-                    {Math.round(progress)}%
+                <div className="skill-level" style={{ width: `${progress4}%` }}>
+                    {Math.round(progress4)}%
                 </div>
             </div>
         </div>
