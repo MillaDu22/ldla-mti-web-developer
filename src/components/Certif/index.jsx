4b030b06cbc5d03c2c785c8f7dd64ea01cf84f90
Milla22 <marti.ludmilla@orange.fr>
2024-06-11 10:42:29 +0200

    certifications google drive

--- src/components/Certif/index.jsx
+++ src/components/Certif/index.jsx
@@ -12,99 +12,99 @@ function Certif() {
                 </li>
                 <li className="li-certif">
                     <strong>Creer un site web avec HTML5 et CSS3</strong>
-                    <p><a className= "lien-certif" href = "https://openclassrooms.com/fr/course-certificates/3638376655" target="blank">Voir le certificat</a></p>
+                    <p><a className= "lien-certif" href = "https://drive.google.com/file/d/1L37j8RETUWA3MEMi-nh9VTcUqbyHdjFP/view?usp=sharing" target="blank">Voir le certificat</a></p>
                 </li>
                 <li className="li-certif">
                     <strong>Le css avec Sass</strong>
-                    <p><a className= "lien-certif" href = "https://openclassrooms.com/fr/course-certificates/5852204522" target="blank">Voir le certificat</a></p>
+                    <p><a className= "lien-certif" href = "https://drive.google.com/file/d/10xFaDeQxJaPq2jdfSOzH7S0b-6Ca6_5I/view?usp=sharing" target="blank">Voir le certificat</a></p>
                 </li>
                 <li className="li-certif">
                     <strong>Programmer avec JavaScript</strong>
-                    <p><a className= "lien-certif" href = "https://openclassrooms.com/fr/course-certificates/4614307780" target="blank">Voir le certificat</a></p>
+                    <p><a className= "lien-certif" href = "https://drive.google.com/file/d/1GeSzdWdHkaqYzzI4OLYsetbX6yjeNnAy/view?usp=sharing" target="blank">Voir le certificat</a></p>
                 </li>
                 <li className="li-certif">
                     <strong>Creer des animations css modernes</strong>
-                    <p><a className= "lien-certif" href = "https://openclassrooms.com/fr/course-certificates/3744628093" target="blank">Voir le certificat</a></p>
+                    <p><a className= "lien-certif" href = "https://drive.google.com/file/d/1UHA3RrujqFtl-jXzIBcemx-dRYWLHHm_/view?usp=sharing" target="blank">Voir le certificat</a></p>
                 </li>
                 <li className="li-certif">
                     <strong>Gerer du code avec Git et GitHub</strong>
-                    <p><a className= "lien-certif" href = "https://openclassrooms.com/fr/course-certificates/5568821645" target="blank">Voir le certificat</a></p>
+                    <p><a className= "lien-certif" href = "https://drive.google.com/file/d/1S9fLw7WBkUy_Gvhii_Fk098aStj25BcW/view?usp=sharing" target="blank">Voir le certificat</a></p>
                 </li>
                 <li className="li-certif">
                     <strong>Utiliser la ligne de commande dans un terminal</strong>
-                    <p><a className= "lien-certif" href = "https://openclassrooms.com/fr/course-certificates/4581085720" target="blank">Voir le certificat</a></p>
+                    <p><a className= "lien-certif" href = "https://drive.google.com/file/d/1ff__39WfcLoX8nKtddDV7KXyUc2Sky5P/view?usp=sharing" target="blank">Voir le certificat</a></p>
                 </li>
                 <li className="li-certif">
                     <strong>Le fonctionnement des algorithmes</strong>
-                    <p><a className= "lien-certif" href = "https://openclassrooms.com/fr/course-certificates/1430244560" target="blank">Voir le certificat</a></p>
+                    <p><a className= "lien-certif" href = "https://drive.google.com/file/d/1A0SATcJrwxRzf-6_T0u9EkgSbm4RV0f_/view?usp=sharing" target="blank">Voir le certificat</a></p>
                 </li>
                 <li className="li-certif">
                     <strong>Ecrire du JavaScript pour le web</strong>
-                    <p><a className= "lien-certif" href = "https://openclassrooms.com/fr/course-certificates/2389353375" target="blank">Voir le certificat</a></p>
+                    <p><a className= "lien-certif" href = "https://drive.google.com/file/d/1QzUU816Jpry5Q96NCMlo8WG7aD9W3MYd/view?usp=sharing" target="blank">Voir le certificat</a></p>
                 </li>
                 <li className="li-certif">
                     <strong>Développeur agile</strong>
-                    <p><a className= "lien-certif" href = "https://openclassrooms.com/fr/course-certificates/9779431406" target="blank">Voir le certificat</a></p>
+                    <p><a className= "lien-certif" href = "https://drive.google.com/file/d/1tOHRxqjMW8Fy4uZWGbBk9Oi1ZkNntX8U/view?usp=sharing" target="blank">Voir le certificat</a></p>
                 </li>
                 <li className="li-certif">
                     <strong>React</strong>
-                    <p><a className= "lien-certif" href = "https://openclassrooms.com/fr/course-certificates/1299435073" target="blank">Voir le certificat</a></p>
+                    <p><a className= "lien-certif" href = "https://drive.google.com/file/d/1xDn0P9H5T5IKTFbLLbbTjQt6ug1qWU-K/view?usp=sharing" target="blank">Voir le certificat</a></p>
                 </li>
                 <li className="li-certif">
                     <strong>Optimisation du référencement d'un site (SEO) en améliorant les performances techniques</strong>
-                    <p><a className= "lien-certif" href = "https://openclassrooms.com/fr/course-certificates/7977967610" target="blank">Voir le certificat</a></p>
+                    <p><a className= "lien-certif" href = "https://drive.google.com/file/d/1eLTLsoklPtjEvEP-DJsynXFCDZRII8Cm/view?usp=sharing" target="blank">Voir le certificat</a></p>
                 </li>
                 <li className="li-certif">
                     <strong>L'accessibilité</strong>
-                    <p><a className= "lien-certif" href = "https://openclassrooms.com/fr/course-certificates/4875036124" target="blank">Voir le certificat</a></p>
+                    <p><a className= "lien-certif" href = "https://drive.google.com/file/d/1X1hkRWqMQl3Fib0G0IW_exITAAUkCoCx/view?usp=sharing" target="blank">Voir le certificat</a></p>
                 </li>
                 <li className="li-certif">
                     <strong>Tester et déboguer l'interface d'un site</strong>
-                    <p><a className= "lien-certif" href = "https://openclassrooms.com/fr/course-certificates/8325307532" target="blank">Voir le certificat</a></p>
+                    <p><a className= "lien-certif" href = "https://drive.google.com/file/d/12c6ylwufoqh6bjmahQuDZMyNSHdK0Qu6/view?usp=sharing" target="blank">Voir le certificat</a></p>
                 </li>
                 <li className="li-certif">
                     <strong>Utiliser le state manager Redux pour gérer l’état de mes applications</strong>
-                    <p><a className= "lien-certif" href = "https://openclassrooms.com/fr/course-certificates/9973355833" target="blank">Voir le certificat</a></p>
+                    <p><a className= "lien-certif" href = "https://drive.google.com/file/d/1q9WvvPV6vCl2Rkr9kgFf8LUC3QlR6k1Z/view?usp=sharing" target="blank">Voir le certificat</a></p>
                 </li>
                 <li className="li-certif">
                     <strong>Mettre en ligne mon site web</strong>
-                    <p><a className= "lien-certif" href = "https://openclassrooms.com/fr/course-certificates/6277851977" target="blank">Voir le certificat</a></p>
+                    <p><a className= "lien-certif" href = "https://drive.google.com/file/d/1OF59XwfjQp2yX2879l3GcpkxrU7oZypA/view?usp=sharing" target="blank">Voir le certificat</a></p>
                 </li>
                 <li className="li-certif">
                     <strong>Utiliser ChatGPT pour améliorer ma productivité</strong>
-                    <p><a className= "lien-certif" href = "https://openclassrooms.com/fr/course-certificates/5766575595" target="blank">Voir le certificat</a></p>
+                    <p><a className= "lien-certif" href = "https://drive.google.com/file/d/1SxyG8gehm3SHxuBCDT0ws6GCgv4uBl49/view?usp=sharing" target="blank">Voir le certificat</a></p>
                 </li>
                 <li className="li-certif">
                     <strong>Passer au Full Stack avec Node.js, Express et MongoDB</strong>
-                    <p><a className= "lien-certif" href = "https://openclassrooms.com/fr/course-certificates/6457821996" target="blank">Voir le certificat</a></p>
+                    <p><a className= "lien-certif" href = "https://drive.google.com/file/d/1k_-aTA7pOj0LA8t9kXrnzlfvauSigNkw/view?usp=sharing" target="blank">Voir le certificat</a></p>
                 </li>
                 <li className="li-certif">
                     <strong>Adopter les API REST pour mes projets web</strong>
-                    <p><a className= "lien-certif" href = "https://openclassrooms.com/fr/course-certificates/9759651794" target="blank">Voir le certificat</a></p>
+                    <p><a className= "lien-certif" href = "https://drive.google.com/file/d/1GmNMiPDk8iIRnnk8SY9m_4rJXgjLYcXa/view?usp=sharing" target="blank">Voir le certificat</a></p>
                 </li>
                 <li className="li-certif">
                     <strong>Appliquer les principes du Green IT dans mon entreprise</strong>
-                    <p><a className= "lien-certif" href = "https://openclassrooms.com/fr/course-certificates/6740933405" target="blank">Voir le certificat</a></p>
+                    <p><a className= "lien-certif" href = "https://drive.google.com/file/d/1O8rcUhXBF0qT9fU1x0nOw2CoZfKaK8vV/view?usp=sharing" target="blank">Voir le certificat</a></p>
                 </li>
                 <li className="li-certif">
                     <strong>Créer des sites web responsives avec Bootstrap 5</strong>
-                    <p><a className= "lien-certif" href = "https://openclassrooms.com/fr/course-certificates/9279673309" target="blank">Voir le certificat</a></p>
+                    <p><a className= "lien-certif" href = "https://drive.google.com/file/d/1tm6cy-KZswWLO6Z6EsNGLwAVmAPrNsR4/view?usp=sharing" target="blank">Voir le certificat</a></p>
                 </li>
                 <li className="li-certif">
                     <strong>Créer une maquette web avec Figma</strong>
-                    <p><a className= "lien-certif" href = "https://openclassrooms.com/fr/course-certificates/7809078989" target="blank">Voir le certificat</a></p>
+                    <p><a className= "lien-certif" href = "https://drive.google.com/file/d/1HtlNqSbzWoIPHWCBb71twXo7EsVSO17h/view?usp=sharing" target="blank">Voir le certificat</a></p>
                 </li>
                 <li className="li-certif">
                     <strong>Réaliser un cahier des charges fonctionnel</strong>
-                    <p><a className= "lien-certif" href = "https://openclassrooms.com/fr/course-certificates/4804239846" target="blank">Voir le certificat</a></p>
+                    <p><a className= "lien-certif" href = "https://drive.google.com/file/d/1NOwwDoDCblth0g8x1H4M1z_s1ztcYi5D/view?usp=sharing" target="blank">Voir le certificat</a></p>
                 </li>
                 <li className="li-certif">
                     <strong>Utiliser des design patterns en JavaScript</strong>
-                    <p><a className= "lien-certif" href = "https://openclassrooms.com/fr/course-certificates/2576888646" target="blank">Voir le certificat</a></p>
+                    <p><a className= "lien-certif" href = "https://drive.google.com/file/d/1627UH9yjMxErOzjCF66StPF-nE3PmES5/view?usp=sharing" target="blank">Voir le certificat</a></p>
                 </li>
                 <li className="li-certif">
                     <strong>Corriger les bugs et trouver une solution à mes problèmes de code</strong>
-                    <p><a className= "lien-certif" href = "https://openclassrooms.com/fr/course-certificates/1820958245" target="blank">Voir le certificat</a></p>
+                    <p><a className= "lien-certif" href = "https://drive.google.com/file/d/1F-Zj1trn-tV6xon2BU5AoMpdEfRlUZun/view?usp=sharing" target="blank">Voir le certificat</a></p>
                 </li>
             </ol>
         </div>
