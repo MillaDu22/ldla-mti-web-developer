588071c711c0881778df95512186782ff14a3876
Milla22 <marti.ludmilla@orange.fr>
2024-07-12 01:32:00 +0200

    counter API

--- src/components/VisitCounter/index.jsx
+++ src/components/VisitCounter/index.jsx
@@ -1,18 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import "./visitcounter.css";
 
 function VisitCounter() {
-  const [count, setCount] = useState(0);
-  useEffect(()=> {
-    const storedCount = localStorage.getItem("pageVisits");
-    const initialCount = Number(storedCount) || 0;
-    setCount(initialCount + 1);
-    localStorage.setItem("pageVisits", initialCount + 1);
-  }, []);
-  
-  return <div className="visit-counter">
-            <i className="fa-solid fa-eye"></i>
-            <p id="visits">{count}</p>
-          </div>
-  }
+  return (
+    <div className="visit-counter">
+      <div className="counterapi"></div>
+    </div>
+  )
+}
 export default VisitCounter;
+
